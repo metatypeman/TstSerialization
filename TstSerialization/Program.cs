ace1a436d53e295ff88c94ead9d333c2376c098e
metatypeman <metatypeman@gmail.com>
2024-07-19 18:50:21 +0300

    setup project

--- TstSerialization/Program.cs
+++ TstSerialization/Program.cs
@@ -1,10 +1,21 @@
-﻿namespace TstSerialization
+﻿using NLog;
+
+namespace TstSerialization
 {
     internal class Program
     {
+        private static ILogger _logger = LogManager.GetCurrentClassLogger();
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+
+            _logger.Info("Hello, World!");
+        }
+
+        private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            _logger.Info($"e.ExceptionObject = {e.ExceptionObject}");
         }
     }
 }
